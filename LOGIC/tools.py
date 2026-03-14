d664181c68fc385c2600ab238f4387d80b0de763
Amine Mustapha <mustaphaamine54@gmail.com>
2026-03-15 00:00:07 +0900

    show emmployyees besed on their salary from higher to the lowest but letting the user to decide the limit

--- LOGIC/tools.py
+++ LOGIC/tools.py
@@ -81,3 +81,52 @@ def search_section(conn):
             department_info[date][department].add_employee(employee, salary)
 
         return department_info
+
+
+def show_top_employees(conn):
+    cursor = conn.cursor()
+
+    while True:
+        employees = {}
+
+        try:
+            user = input(
+                "Enter number of employees to show based on highest salary or 'exit' to quit: "
+            ).strip()
+
+            if user.lower() == "exit":
+                print("Returning to menu...")
+                return None
+
+            user_limit = int(user)
+
+        except ValueError:
+            print("Invalid input.")
+            continue
+
+        cursor.execute("""
+            SELECT com.department, sec.employee, sec.salary
+            FROM company com
+            JOIN sections sec ON com.id = sec.department_id
+            ORDER BY sec.salary DESC
+            LIMIT ?
+        """, (user_limit,))
+
+        rows = cursor.fetchall()
+
+        if not rows:
+            print("No employees found.")
+            continue
+
+        for row in rows:
+            department = row["department"]
+            employee = row["employee"]
+            salary = row["salary"]
+
+            if department not in employees:  # if the department does not exist in the dictionary create a Company object for it
+                employees[department] = Company(department)
+
+            # if it already exists, the if statement is skipped so it will just add employee, salary to the existent department
+            employees[department].add_employee(employee, salary)
+
+        return employees
